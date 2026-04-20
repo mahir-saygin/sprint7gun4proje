91d43c691038250b4e57274114f7b9181de5106a
mahir-saygin <mahirsaygin1986@gmail.com>
2026-04-20 12:54:29 +0100

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='read-the-docs'>Merhaba</p>
+    </>
+  )
+}
+
+export default App
